48402cfd258c00bf663cb245fe279c2c44bd69df
Md. Abraul Hoque <154797244+abraul583@users.noreply.github.com>
2026-08-24 17:00:23 +0600

    rock_paper_scissors_lizard_spock.cpp

--- rock_paper_scissors_lizard_spock.cpp
+++ rock_paper_scissors_lizard_spock.cpp
@@ -3,8 +3,11 @@
 Rock, Paper, Scissors, Lizard, Spock
 This is the infamous game brought to popularity with the TV show The Big Bang Theory.
 
-So, the rules are:  1.Scissors cuts paper 2.paper covers rock 3.rock crushes lizard 4.lizard poison's spock 5.spock smashes scissors 6.scissors decapitates lizard 7.lizard eats paper 8.paper disproves spock
-9.spock vaporizes rock 10.rock crushes scissors
+So, the rules are:  1.Scissors cuts paper     2.paper covers rock 
+                    3.rock crushes lizard     4.lizard poison's spock 
+                    5.spock smashes scissors  6.scissors decapitates lizard 
+                    7.lizard eats paper       8.paper disproves spock
+                    9.spock vaporizes rock    10.rock crushes scissors
 
 I will compare the user's choice and the computer's chpoice and determine the winner.
 */
