ff3add0f80f8c8bbc620069b341cd29e2c721654
Md. Abraul Hoque <154797244+abraul583@users.noreply.github.com>
2026-08-24 16:58:24 +0600

    Create rock_paper_scissors_lizard_spook.cpp

---
+++ rock_paper_scissors_lizard_spook.cpp
@@ -0,0 +1,19 @@
+/*It seems like when you know someone well enough, 75-80% of any Rock Paper Scissors games you play with that person end up in a tie.
+
+Rock, Paper, Scissors, Lizard, Spock
+This is the infamous game brought to popularity with the TV show The Big Bang Theory.
+
+So, the rules are:  1.Scissors cuts paper 2.paper covers rock 3.rock crushes lizard 4.lizard poison's spock 5.spock smashes scissors 6.scissors decapitates lizard 7.lizard eats paper 8.paper disproves spock
+9.spock vaporizes rock 10.rock crushes scissors
+
+I will compare the user's choice and the computer's chpoice and determine the winner.
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+  
+}
