5de644f60ef83ec9de21cd1edad79ebf40d930a2
Md. Abraul Hoque <154797244+abraul583@users.noreply.github.com>
2026-08-26 15:05:44 +0600

    whale_language.cpp

--- whale_language.cpp
+++ whale_language.cpp
@@ -1,5 +1,5 @@
 /*
-Takes a phrase like "turpentine and turtles" and translate it into its “whale talk” equivalent, "uueeieeauuee".
+Takes a phrase like "turpentine and turtles" or anything and translate it into its “whale talk” equivalent, "uueeieeauuee".
 
 There are a few simple rules for translating text to whale language:
 
@@ -11,17 +11,23 @@ Once we have converted text to the whale language, the result is sung slowly, as
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cctype>
 using namespace std;
 int main() 
 {
-  string input="a whale of a deal!"; // to be translated into whale language.
-  vector<char> vowels={'a','e','i','o','u'}; //Whales only speak in vowels
+  string input;// to be translated into whale language.
+  cout << "Enter text to translate: ";
+  getline(cin,input);
+
+  vector<char> vowels={'a','e','i','o','u'};//Whales only speak in vowels
   vector<char> result; //This will serve as a place to store the vowels from the input string
   for (int i=0; i<input.length(); i++)
-  {
+  { 
+    char c=tolower(input[i]);//first the input character is lowered here.
+
     for(int j=0; j<vowels.size(); j++)
     {
-        if(input[i]==vowels[j])
+        if(c==vowels[j]) 
         {
           result.push_back(vowels[j]);
           if(vowels[j]=='u' || vowels[j]=='e')
@@ -31,10 +37,11 @@ int main()
         }
     }
   }
-
+cout<<"Whale says: ";
   for(int k=0; k<result.size(); k++)
   {
     cout<<result[k];
   }
 cout<<endl;
 }
+
