ebde84c2325cf3d021187e4ebe80dd8bfe5a0e83
Md. Abraul Hoque <154797244+abraul583@users.noreply.github.com>
2026-08-28 13:22:49 +0600

    Create ufo_functions.cpp

---
+++ ufo_functions.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// Define functions
+void greet(){
+
+  cout<<"============="<<endl;
+  cout<<"UFO: The Game"<<endl;
+  cout<<"============="<<endl;
+  cout<<"Instructions: save your friend from alien abduction by"; cout<<"guessing the letters in the codeword."<<endl;
+}
+
+
+void end_game(string codeword,string answer)
+{
+  if(answer==codeword)
+{
+  cout<<"Hooray! You saved the person and earned a medal of honor !"<<"\n";
+}
+else
+{
+  cout<<"Oh no! The UFO just flew away with another person!"<<endl;
+  cout<<"\n"<<"\n"<<"The right codeword was "<<codeword<<endl;
+}
+}
+
+
+void display_status(vector<char> incorrect, string answer)
+{
+  cout<<"Incorrect Guesses: ";
+  for(int i=0; i<incorrect.size(); i++)
+  {
+    cout<<incorrect[i]<<" ";
+  }
+  cout<<endl;
+
+  cout<<"Codeword: ";
+  for(int j=0; j<answer.length(); j++)
+  {
+    cout<<answer[j]<<" ";
+  }
+  cout<<endl;
+}
+
+
+
+void display_misses(int misses) {
+
+  if (misses == 0 || misses == 1) {
+
+    cout << "                 .                            \n";
+    cout << "                 |                            \n";
+    cout << "              .-\"^\"-.                       \n";
+    cout << "             /_....._\\                       \n";
+    cout << "         .-\"`         `\"-.                  \n";
+    cout << "        (  ooo  ooo  ooo  )                   \n";
+    cout << "         '-.,_________,.-'    ,-----------.   \n";
+    cout << "              /     \\        (  Send help! ) \n";
+    cout << "             /   0   \\      / `-----------'  \n";
+    cout << "            /  --|--  \\    /                 \n";
+    cout << "           /     |     \\                     \n";
+    cout << "          /     / \\     \\                   \n";
+    cout << "         /               \\                   \n";
+
+  }
+  else if (misses == 2) {
+
+    cout << "                 .                            \n";
+    cout << "                 |                            \n";
+    cout << "              .-\"^\"-.                       \n";
+    cout << "             /_....._\\                       \n";
+    cout << "         .-\"`         `\"-.                  \n";
+    cout << "        (  ooo  ooo  ooo  )                   \n";
+    cout << "         '-.,_________,.-'    ,-----------.   \n";
+    cout << "              /     \\        (  Send help! ) \n";
+    cout << "             /   0   \\      / `-----------'  \n";
+    cout << "            /  --|--  \\    /                 \n";
+    cout << "           /     |     \\                     \n";
+    cout << "          /     / \\     \\                   \n";
+    cout << "         /               \\                   \n";
+
+  }
+  else if (misses == 3) {
+
+   cout << "                 .                            \n";
+    cout << "                 |                            \n";
+    cout << "              .-\"^\"-.                       \n";
+    cout << "             /_....._\\                       \n";
+    cout << "         .-\"`         `\"-.                  \n";
+    cout << "        (  ooo  ooo  ooo  )                   \n";
+    cout << "         '-.,_________,.-'    ,-----------.   \n";
+    cout << "              /     \\        (  Send help! ) \n";
+    cout << "             /   0   \\      / `-----------'  \n";
+    cout << "            /  --|--  \\    /                 \n";
+    cout << "           /     |     \\                     \n";
+    cout << "          /     / \\     \\                   \n";
+    cout << "         /               \\                   \n";
+
+  }
+  else if (misses == 3) {
+
+    cout << "                 .                            \n";
+    cout << "                 |                            \n";
+    cout << "              .-\"^\"-.                       \n";
+    cout << "             /_....._\\                       \n";
+    cout << "         .-\"`         `\"-.                  \n";
+    cout << "        (  ooo  ooo  ooo  )                   \n";
+    cout << "         '-.,_________,.-'    ,-----------.   \n";
+    cout << "              /     \\        (  Send help! ) \n";
+    cout << "             /   0   \\      / `-----------'  \n";
+    cout << "            /  --|--  \\    /                 \n";
+    cout << "           /     |     \\                     \n";
+    cout << "          /     / \\     \\                   \n";
+    cout << "         /               \\                   \n";
+
+  }
+  else if (misses == 4) {
+
+    cout << "                 .                            \n";
+    cout << "                 |                            \n";
+    cout << "              .-\"^\"-.                       \n";
+    cout << "             /_....._\\                       \n";
+    cout << "         .-\"`         `\"-.                  \n";
+    cout << "        (  ooo  ooo  ooo  )                   \n";
+    cout << "         '-.,_________,.-'    ,-----------.   \n";
+    cout << "              /     \\        (  Send help! ) \n";
+    cout << "             /   0   \\      / `-----------'  \n";
+    cout << "            /  --|--  \\    /                 \n";
+    cout << "           /     |     \\                     \n";
+    cout << "          /     / \\     \\                   \n";
+    cout << "         /               \\                   \n";
+
+  }
+  else if (misses == 5) {
+
+    cout << "                 .                            \n";
+    cout << "                 |                            \n";
+    cout << "              .-\"^\"-.                       \n";
+    cout << "             /_....._\\                       \n";
+    cout << "         .-\"`         `\"-.                  \n";
+    cout << "        (  ooo  ooo  ooo  )                   \n";
+    cout << "         '-.,_________,.-'    ,-----------.   \n";
+    cout << "              /     \\        (  Send help! ) \n";
+    cout << "             /   0   \\      / `-----------'  \n";
+    cout << "            /  --|--  \\    /                 \n";
+    cout << "           /     |     \\                     \n";
+    cout << "          /     / \\     \\                   \n";
+    cout << "         /               \\                   \n";
+
+  }
+  else if (misses == 6) {
+
+    cout << "                 .                            \n";
+    cout << "                 |                            \n";
+    cout << "              .-\"^\"-.                       \n";
+    cout << "             /_....._\\                       \n";
+    cout << "         .-\"`         `\"-.                  \n";
+    cout << "        (  ooo  ooo  ooo  )                   \n";
+    cout << "         '-.,_________,.-'    ,-----------.   \n";
+    cout << "              /     \\        (  Send help! ) \n";
+    cout << "             /   0   \\      / `-----------'  \n";
+    cout << "            /  --|--  \\    /                 \n";
+    cout << "           /     |     \\                     \n";
+    cout << "          /     / \\     \\                   \n";
+    cout << "         /               \\                   \n";
+
+  }
+
+}
