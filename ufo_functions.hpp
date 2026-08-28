f211907450197c224890ed58bc78fdf6b047f971
Md. Abraul Hoque <154797244+abraul583@users.noreply.github.com>
2026-08-28 13:22:21 +0600

    Create ufo_functions.hpp

---
+++ ufo_functions.hpp
@@ -0,0 +1,8 @@
+#include <vector>
+using namespace std;
+
+void display_misses(int misses);
+void greet();
+void end_game(string codeword,string answer);
+void display_status(vector<char> incorrect, string answer);
+
